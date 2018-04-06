e7242aac312b25620aedb39a4599438ed8bf01f3
mrs <tomas.baca@fel.cvut.cz>
2018-04-06 15:05:58 +0200

    added service calls for net gun, fixed garmin namesapce

--- include/garmin/garmin.h
+++ include/garmin/garmin.h
@@ -1,18 +1,17 @@
 #ifndef GARMIN_H_
 #define GARMIN_H_
 
+#include <ros/package.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Range.h>
 #include <std_msgs/Char.h>
+#include <std_srvs/Trigger.h>
 
 #include <string>
 
 #include "garmin/serial_port.h"
 
 #define BUFFER_SIZE 64
-
-namespace garmin
-{
 
   //static const char PRECISE_MODE = 'P';
 
@@ -24,6 +23,15 @@ namespace garmin
 
       uint8_t crc8(uint8_t *p, uint8_t len);
       void serialDataCallback(uint8_t data);
+
+      ros::ServiceServer netgun_arm;
+      ros::ServiceServer netgun_safe;
+      ros::ServiceServer netgun_fire;
+
+      bool netgun_safeCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+      bool netgun_armCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+      bool netgun_fireCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+
 
       bool loadParameters();
       void setMode(char c);
@@ -41,7 +49,5 @@ namespace garmin
 
       std::string portname_;
   };
-
-} // namespace garmin
 
 #endif  // GARMIN_H_
--- src/garmin.cpp
+++ src/garmin.cpp
@@ -1,161 +1,218 @@
 #include <string>
 
+#include <std_srvs/Trigger.h>
 #include "garmin/garmin.h"
 
 #define MAXIMAL_TIME_INTERVAL 1
 
-namespace garmin
-{
+Garmin::Garmin() {
+  // Get paramters
+  ros::NodeHandle private_node_handle_("~");
+  private_node_handle_.param("portname", portname_, std::string("/dev/ttyUSB0"));
 
-  Garmin::Garmin()
-  {
-    // Get paramters
-    ros::NodeHandle private_node_handle_("~");
-    private_node_handle_.param("portname", portname_, std::string("/dev/ttyUSB0"));
+  // Publishers
+  range_publisher_ = nh_.advertise<sensor_msgs::Range>("range", 1);
 
-    // Publishers
-    range_publisher_ = nh_.advertise<sensor_msgs::Range>("range", 1);
+  netgun_arm       = nh_.advertiseService("netgun_arm", &Garmin::netgun_armCallback, this);
+  netgun_safe      = nh_.advertiseService("netgun_safe", &Garmin::netgun_safeCallback, this);
+  netgun_fire      = nh_.advertiseService("netgun_fire", &Garmin::netgun_fireCallback, this);
 
-    // Output loaded parameters to console for double checking
-    ROS_INFO("[%s] is up and running with the following parameters:", ros::this_node::getName().c_str());
-    ROS_INFO("[%s] portname: %s", ros::this_node::getName().c_str(), portname_.c_str());
 
-    connectToSensor();
+  // Output loaded parameters to console for double checking
+  ROS_INFO("[%s] is up and running with the following parameters:", ros::this_node::getName().c_str());
+  ROS_INFO("[%s] portname: %s", ros::this_node::getName().c_str(), portname_.c_str());
+
+  connectToSensor();
+}
+
+Garmin::~Garmin() {
+}
+
+bool Garmin::netgun_safeCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+
+  char id      = '1';
+  char tmpSend = 'a';
+  char crc     = tmpSend;
+
+  serial_port_->sendChar(tmpSend);
+  tmpSend = 1;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  tmpSend = id;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  serial_port_->sendChar(crc);
+
+  ROS_INFO("Safing net gun");
+  res.message = "Safing net gun";
+  res.success = true;
+  return true;
+}
+
+bool Garmin::netgun_armCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+
+  char id      = '2';
+  char tmpSend = 'a';
+  char crc     = tmpSend;
+
+  serial_port_->sendChar(tmpSend);
+  tmpSend = 1;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  tmpSend = id;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  serial_port_->sendChar(crc);
+
+  ROS_INFO("Safing net gun");
+  res.message = "Safing net gun";
+  res.success = true;
+  return true;
+}
+
+bool Garmin::netgun_fireCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+
+  char id      = '3';
+  char tmpSend = 'a';
+  char crc     = tmpSend;
+
+  serial_port_->sendChar(tmpSend);
+  tmpSend = 1;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  tmpSend = id;
+  crc += tmpSend;
+  serial_port_->sendChar(tmpSend);
+  serial_port_->sendChar(crc);
+
+  ROS_INFO("Safing net gun");
+  res.message = "Safing net gun";
+  res.success = true;
+  return true;
+}
+
+uint8_t Garmin::connectToSensor(void) {
+
+  // Create serial port
+  serial_port_ = new serial_device::SerialPort();
+
+  // Set callback function for the serial ports
+  serial_data_callback_function_ = boost::bind(&Garmin::serialDataCallback, this, _1);
+  serial_port_->setSerialCallbackFunction(&serial_data_callback_function_);
+
+  // Connect serial port
+  if (!serial_port_->connect(portname_)) {
+    ROS_ERROR("Could not connect to sensor.");
+    return 0;
   }
 
-  Garmin::~Garmin()
-  {
-  }
+  ROS_INFO("Connected to sensor.");
 
-  uint8_t Garmin::connectToSensor(void) {
+  lastReceived = ros::Time::now();
 
-    // Create serial port
-    serial_port_ = new serial_device::SerialPort();
+  return 1;
+}
 
-    // Set callback function for the serial ports
-    serial_data_callback_function_ = boost::bind(&Garmin::serialDataCallback, this, _1);
-    serial_port_->setSerialCallbackFunction(&serial_data_callback_function_);
+void Garmin::releaseSerialLine(void) {
 
-    // Connect serial port
-    if (!serial_port_->connect(portname_))
-    {
-      ROS_ERROR("Could not connect to sensor.");
-      return 0;
-    }
+  delete serial_port_;
+}
 
-    ROS_INFO("Connected to sensor.");	
-
-    lastReceived = ros::Time::now();
-
-    return 1;
-  }
-
-  void Garmin::releaseSerialLine(void) {
-
-    delete serial_port_;
-  }
-
-  void Garmin::serialDataCallback(uint8_t single_character)
-  {
-    static uint8_t input_buffer[BUFFER_SIZE];
-    static int buffer_ctr = 0;
-    static uint8_t crc_in = 0;
-    static int payload_size = 0;
-    static int receiver_state = 0;
-    static int receiving_message = 0;
+void Garmin::serialDataCallback(uint8_t single_character) {
+  static uint8_t input_buffer[BUFFER_SIZE];
+  static int     buffer_ctr        = 0;
+  static uint8_t crc_in            = 0;
+  static int     payload_size      = 0;
+  static int     receiver_state    = 0;
+  static int     receiving_message = 0;
 
 
-    if (receiving_message==1) {
+  if (receiving_message == 1) {
 
-      // expecting to receive the payload size
-      if (receiver_state == 0) {
+    // expecting to receive the payload size
+    if (receiver_state == 0) {
 
-        // check the message length
-        if (single_character >= 0 && single_character < 64) {
+      // check the message length
+      if (single_character >= 0 && single_character < 64) {
 
-          payload_size = single_character;
-          receiver_state = 1;
-          crc_in += single_character;
-
-          // the receiving message is over the buffer size
-        } else {
-
-          receiving_message = 0;
-          receiver_state = 0;
-          ROS_WARN("[%s] reveived expected size out of bounds <0,63>, reset buffer without evaluating data", ros::this_node::getName().c_str());
-        }
-
-        // expecting to receive the payload
-      } else if (receiver_state == 1) {
-
-        // put the char in the buffer
-        input_buffer[buffer_ctr++] = single_character;
-        // add crc
+        payload_size   = single_character;
+        receiver_state = 1;
         crc_in += single_character;
 
-        // if the message should end, change state
-        if (buffer_ctr >= payload_size)
-          receiver_state = 2;
+        // the receiving message is over the buffer size
+      } else {
 
-        // expecting to receive the crc
-      } else if (receiver_state == 2) {
-
-        if (crc_in == single_character) {
-          receiving_message = 0;
-
-          // just int16
-          // input_buffer[0] message_id
-          int16_t range = input_buffer[1] << 8;
-          range |= input_buffer[2];
-
-          if (range < 4000 && range >= 0)
-          {
-
-            sensor_msgs::Range range_msg;
-            range_msg.field_of_view = 0.0523599; // +-3 degree
-            range_msg.max_range = 40.0;
-            range_msg.min_range = 0;
-            range_msg.radiation_type = sensor_msgs::Range::INFRARED;
-            range_msg.header.frame_id = "garmin_frame";
-            range_msg.header.stamp = ros::Time::now();
-            range_msg.range = range * 0.01; // convert to m
-            range_publisher_.publish(range_msg);
-            lastReceived = ros::Time::now();
-
-          }
-          ROS_DEBUG("[%s] all good %.3f m", ros::this_node::getName().c_str(),range*0.01);
-
-        } else {
-          ROS_DEBUG("[%s] crc missmatch", ros::this_node::getName().c_str());
-          receiving_message = 0;
-          receiver_state = 0;
-        }
+        receiving_message = 0;
+        receiver_state    = 0;
+        ROS_WARN("[%s] reveived expected size out of bounds <0,63>, reset buffer without evaluating data", ros::this_node::getName().c_str());
       }
 
-    } else {
-      // this character precedes every message
-      if (single_character == 'b') {
+      // expecting to receive the payload
+    } else if (receiver_state == 1) {
 
-        receiving_message = 1;
-        receiver_state = 0;
-        buffer_ctr = 0;
-        crc_in = single_character;
+      // put the char in the buffer
+      input_buffer[buffer_ctr++] = single_character;
+      // add crc
+      crc_in += single_character;
+
+      // if the message should end, change state
+      if (buffer_ctr >= payload_size)
+        receiver_state = 2;
+
+      // expecting to receive the crc
+    } else if (receiver_state == 2) {
+
+      if (crc_in == single_character) {
+        receiving_message = 0;
+
+        // just int16
+        // input_buffer[0] message_id
+        int16_t range = input_buffer[1] << 8;
+        range |= input_buffer[2];
+
+        if (range < 4000 && range >= 0) {
+
+          sensor_msgs::Range range_msg;
+          range_msg.field_of_view   = 0.0523599;  // +-3 degree
+          range_msg.max_range       = 40.0;
+          range_msg.min_range       = 0;
+          range_msg.radiation_type  = sensor_msgs::Range::INFRARED;
+          range_msg.header.frame_id = "garmin_frame";
+          range_msg.header.stamp    = ros::Time::now();
+          range_msg.range           = range * 0.01;  // convert to m
+          range_publisher_.publish(range_msg);
+          lastReceived = ros::Time::now();
+        }
+        ROS_DEBUG("[%s] all good %.3f m", ros::this_node::getName().c_str(), range * 0.01);
+
+      } else {
+        ROS_DEBUG("[%s] crc missmatch", ros::this_node::getName().c_str());
+        receiving_message = 0;
+        receiver_state    = 0;
       }
     }
+
+  } else {
+    // this character precedes every message
+    if (single_character == 'b') {
+
+      receiving_message = 1;
+      receiver_state    = 0;
+      buffer_ctr        = 0;
+      crc_in            = single_character;
+    }
   }
+}
 
-  void Garmin::setMode(char c)
-  {
-    serial_port_->sendChar(c);
-  }
+void Garmin::setMode(char c) {
+  serial_port_->sendChar(c);
+}
 
-} // namespace garmin
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "Garmin");
-  garmin::Garmin garmin_sensor;
-  ros::Rate loop_rate(1);
+  Garmin garmin_sensor;
+  ros::Rate      loop_rate(1);
 
   while (ros::ok()) {
 
@@ -167,7 +224,7 @@ int main(int argc, char **argv)
 
       ROS_WARN("Garmin not responding, resetting connection...");
 
-      // if establishing the new connection was successfull	
+      // if establishing the new connection was successfull
       if (garmin_sensor.connectToSensor() == 1) {
 
         ROS_WARN("New connection to Garmin was established.");
