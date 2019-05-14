c818f5062b86dec11c939bd96e13d69b81cb34fe
dan <hertdani@fel.cvut.cz>
2019-05-14 17:00:24 +0200

    now accepts external messages

--- src/baca_protocol.cpp
+++ src/baca_protocol.cpp
@@ -1,10 +1,12 @@
 #include <ros/package.h>
+#include <stdlib.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Range.h>
 #include <std_msgs/Char.h>
 #include <std_srvs/Trigger.h>
 #include <std_srvs/SetBool.h>
 #include <std_msgs/Bool.h>
+#include <mutex>
 
 #include <string>
 #include <mrs_msgs/BacaProtocol.h>
@@ -25,7 +27,7 @@ class BacaProtocol {
 public:
   BacaProtocol();
 
-  void serialDataCallback(uint8_t data);
+  void callbackSerialData(uint8_t data);
 
   enum serial_receiver_state
   {
@@ -39,23 +41,11 @@ public:
   ros::ServiceServer netgun_arm;
   ros::ServiceServer netgun_safe;
   ros::ServiceServer netgun_fire;
-  ros::ServiceServer uvled_start_left;
-  ros::ServiceServer uvled_start_right;
-  ros::ServiceServer uvled_stop;
-  ros::ServiceServer board_switch;
-  ros::ServiceServer beacon_on;
-  ros::ServiceServer beacon_off;
 
   bool callbackNetgunSafe(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
   bool callbackNetgunArm(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
   bool callbackNetgunFire(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
-  bool callbackUvLedStartLeft(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
-  bool callbackUvLedStartRight(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
-  bool callbackUvLedStop(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
-  bool callbackBoardSwitch(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
-  bool callbackBeaconOn(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
-  bool callbackBeaconOff(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
-  void fireTopicCallback(const std_msgs::BoolConstPtr &msg);
+  void callbackSendMessage(const mrs_msgs::BacaProtocolConstPtr &msg);
 
   uint8_t connectToSensor(void);
   void    releaseSerialLine(void);
@@ -64,21 +54,19 @@ public:
   ros::Time lastReceived;
 
   ros::NodeHandle nh_;
+
   ros::Publisher  range_publisher_;
   ros::Publisher  range_publisher_up_;
   ros::Publisher  baca_protocol_publisher_;
 
-  ros::Subscriber fire_subscriber;
+  ros::Subscriber baca_protocol_subscriber;
 
   serial_device::SerialPort *    serial_port_;
   boost::function<void(uint8_t)> serial_data_callback_function_;
 
   std::string portname_;
 
-  bool enable_servo_;
-  bool enable_uvleds_;
-  bool enable_switch_;
-  bool enable_beacon_;
+  std::mutex mutex_msg;
 };
 
 //}
@@ -93,43 +81,22 @@ BacaProtocol::BacaProtocol() {
   ros::Time::waitForValid();
 
   nh_.param("portname", portname_, std::string("/dev/ttyUSB0"));
-  nh_.param("enable_servo", enable_servo_, false);
-  nh_.param("enable_uvleds", enable_uvleds_, false);
-  nh_.param("enable_switch", enable_switch_, false);
-  nh_.param("enable_beacon", enable_beacon_, false);
 
   // Publishers
   range_publisher_         = nh_.advertise<sensor_msgs::Range>("range", 1);
   range_publisher_up_      = nh_.advertise<sensor_msgs::Range>("range_up", 1);
   baca_protocol_publisher_ = nh_.advertise<mrs_msgs::BacaProtocol>("baca_protocol_out", 1);
 
-  fire_subscriber = nh_.subscribe("fire_topic", 1, &BacaProtocol::fireTopicCallback, this, ros::TransportHints().tcpNoDelay());
+  baca_protocol_subscriber = nh_.subscribe("baca_protocol_in", 1, &BacaProtocol::callbackSendMessage, this, ros::TransportHints().tcpNoDelay());
 
   // service out
+  netgun_arm  = nh_.advertiseService("netgun_arm", &BacaProtocol::callbackNetgunArm, this);
+  netgun_safe = nh_.advertiseService("netgun_safe", &BacaProtocol::callbackNetgunSafe, this);
+  netgun_fire = nh_.advertiseService("netgun_fire", &BacaProtocol::callbackNetgunFire, this);
 
-  if (enable_servo_) {
-    netgun_arm  = nh_.advertiseService("netgun_arm", &BacaProtocol::callbackNetgunArm, this);
-    netgun_safe = nh_.advertiseService("netgun_safe", &BacaProtocol::callbackNetgunSafe, this);
-    netgun_fire = nh_.advertiseService("netgun_fire", &BacaProtocol::callbackNetgunFire, this);
-  }
-  if (enable_uvleds_) {
-    uvled_start_left  = nh_.advertiseService("uvled_start_left", &BacaProtocol::callbackUvLedStartLeft, this);
-    uvled_start_right = nh_.advertiseService("uvled_start_right", &BacaProtocol::callbackUvLedStartRight, this);
-    uvled_stop        = nh_.advertiseService("uvled_stop", &BacaProtocol::callbackUvLedStop, this);
-  }
-  if (enable_beacon_) {
-    beacon_on  = nh_.advertiseService("beacon_start", &BacaProtocol::callbackBeaconOn, this);
-    beacon_off = nh_.advertiseService("beacon_stop", &BacaProtocol::callbackBeaconOff, this);
-  }
-  if (enable_switch_) {
-    board_switch = nh_.advertiseService("board_switch", &BacaProtocol::callbackBoardSwitch, this);
-  }
   // Output loaded parameters to console for double checking
   ROS_INFO("[%s] is up and running with the following parameters:", ros::this_node::getName().c_str());
   ROS_INFO("[%s] portname: %s", ros::this_node::getName().c_str(), portname_.c_str());
-  ROS_INFO("[%s] enable_servo: %d", ros::this_node::getName().c_str(), enable_servo_);
-  ROS_INFO("[%s] enable_switch: %d", ros::this_node::getName().c_str(), enable_switch_);
-  ROS_INFO("[%s] enable_uvleds: %d", ros::this_node::getName().c_str(), enable_uvleds_);
   lastReceived = ros::Time::now();
 
   connectToSensor();
@@ -137,25 +104,19 @@ BacaProtocol::BacaProtocol() {
 
 //}
 
-// | ------------------------ services ------------------------ |
+// | ------------------------ callbacks ------------------------ |
 
 /*  callbackNetgunSafe()//{ */
 
 bool BacaProtocol::callbackNetgunSafe([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
 
-  char id      = '7';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
+  mrs_msgs::BacaProtocol msg;
+  msg.stamp = ros::Time::now();
+  msg.payload.push_back('7');
 
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
+  mrs_msgs::BacaProtocolConstPtr const_msg(new mrs_msgs::BacaProtocol(msg));
+  callbackSendMessage(const_msg);
+  
   ROS_INFO("Safing net gun");
   res.message = "Safing net gun";
   res.success = true;
@@ -169,19 +130,13 @@ bool BacaProtocol::callbackNetgunSafe([[maybe_unused]] std_srvs::Trigger::Reques
 
 bool BacaProtocol::callbackNetgunArm([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
 
-  char id      = '8';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
+  mrs_msgs::BacaProtocol msg;
+  msg.stamp = ros::Time::now();
+  msg.payload.push_back('8');
 
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
+  mrs_msgs::BacaProtocolConstPtr const_msg(new mrs_msgs::BacaProtocol(msg));
+  callbackSendMessage(const_msg);
+  
   ROS_INFO("Arming net gun");
   res.message = "Arming net gun";
   res.success = true;
@@ -194,20 +149,14 @@ bool BacaProtocol::callbackNetgunArm([[maybe_unused]] std_srvs::Trigger::Request
 /* callbackNetgunFire() //{ */
 
 bool BacaProtocol::callbackNetgunFire([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+  
+  mrs_msgs::BacaProtocol msg;
+  msg.stamp = ros::Time::now();
+  msg.payload.push_back('9');
 
-  char id      = '9';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
+  mrs_msgs::BacaProtocolConstPtr const_msg(new mrs_msgs::BacaProtocol(msg));
+  callbackSendMessage(const_msg);
+  
   ROS_INFO("Firing net gun");
   res.message = "Firing net gun";
   res.success = true;
@@ -216,245 +165,9 @@ bool BacaProtocol::callbackNetgunFire([[maybe_unused]] std_srvs::Trigger::Reques
 
 //}
 
-/* callbackBeaconOn() //{ */
+/* callbackSerialData() //{ */
 
-bool BacaProtocol::callbackBeaconOn([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
-
-  char id      = '4';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Starting sirene and beacon");
-  res.message = "Starting sirene and beacon";
-  res.success = true;
-  return true;
-}
-
-//}
-
-/* callbackBeaconOff() //{ */
-
-bool BacaProtocol::callbackBeaconOff([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
-
-  char id      = '5';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Stopping sirene and beacon");
-  res.message = "Stopping sirene and beacon";
-  res.success = true;
-  return true;
-}
-
-//}
-
-/* callbackUvLedStartLeft() //{ */
-
-bool BacaProtocol::callbackUvLedStartLeft(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res) {
-
-  char id      = '1';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = 2;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = (uint8_t)req.data;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Starting left UV leds. f: %d", req.data);
-  res.message = "Starting left UV leds";
-  res.success = true;
-  return true;
-}
-
-//}
-
-/* callbackUvLedStartRight() //{ */
-
-bool BacaProtocol::callbackUvLedStartRight(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res) {
-
-  char id      = '2';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = 2;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = (uint8_t)req.data;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Starting right UV leds. f: %d", req.data);
-  res.message = "Starting right UV leds";
-  res.success = true;
-
-  return true;
-}
-
-//}
-
-/* callbackUvLedStop() //{ */
-
-bool BacaProtocol::callbackUvLedStop([[maybe_unused]] std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
-
-  char id      = '3';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-  tmpSend = 1;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Stopping UV LEDs");
-  res.message = "Stopping UV LEDs";
-  res.success = true;
-
-  return true;
-}
-
-//}
-
-/* callbackBoardSwitch() //{ */
-
-bool BacaProtocol::callbackBoardSwitch(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res) {
-
-  char id      = '4';
-  char tmpSend = 'a';
-  char crc     = tmpSend;
-
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = 2;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = id;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  tmpSend = (uint8_t)req.data;
-  crc += tmpSend;
-  serial_port_->sendChar(tmpSend);
-
-  serial_port_->sendChar(crc);
-
-  ROS_INFO("Switching output to: %d", req.data);
-  res.message = "Output switched";
-  res.success = true;
-
-  return true;
-}
-
-//}
-
-/* fireTopicCallback //{ */
-
-void BacaProtocol::fireTopicCallback(const std_msgs::BoolConstPtr &msg) {
-  std_msgs::Bool mybool = *msg;
-  if (mybool.data) {
-    char id      = '9';
-    char tmpSend = 'a';
-    char crc     = tmpSend;
-
-    serial_port_->sendChar(tmpSend);
-    tmpSend = 1;
-    crc += tmpSend;
-    serial_port_->sendChar(tmpSend);
-    tmpSend = id;
-    crc += tmpSend;
-    serial_port_->sendChar(tmpSend);
-    serial_port_->sendChar(crc);
-
-    ROS_INFO("Firing net gun");
-  }
-}
-
-//}
-
-// |                          routines                          |
-
-/* connectToSensors() //{ */
-
-uint8_t BacaProtocol::connectToSensor(void) {
-
-  // Create serial port
-  serial_port_ = new serial_device::SerialPort();
-
-  // Set callback function for the serial ports
-  serial_data_callback_function_ = boost::bind(&BacaProtocol::serialDataCallback, this, _1);
-  serial_port_->setSerialCallbackFunction(&serial_data_callback_function_);
-
-  // Connect serial port
-  ROS_INFO("[%s]: Openning the serial port.", ros::this_node::getName().c_str());
-  if (!serial_port_->connect(portname_)) {
-    ROS_ERROR("[%s]: Could not connect to sensor.", ros::this_node::getName().c_str());
-    return 0;
-  }
-
-  ROS_INFO("[%s]: Connected to sensor.", ros::this_node::getName().c_str());
-
-  lastReceived = ros::Time::now();
-
-  return 1;
-}
-
-//}
-
-/* releaseSerialLine() //{ */
-
-void BacaProtocol::releaseSerialLine(void) {
-
-  delete serial_port_;
-}
-
-//}
-
-/* serialDataCallback() //{ */
-
-void BacaProtocol::serialDataCallback(uint8_t single_character) {
+void BacaProtocol::callbackSerialData(uint8_t single_character) {
 
   static serial_receiver_state rec_state    = WAITING_FOR_MESSSAGE;
   static uint8_t               payload_size = 0;
@@ -502,7 +215,7 @@ void BacaProtocol::serialDataCallback(uint8_t single_character) {
         lastReceived = ros::Time::now();
         rec_state    = WAITING_FOR_MESSSAGE;
       } else {
-        ROS_ERROR_STREAM("[ " << ros::this_node::getName().c_str() << "]: Message with bad crc received, received: " << static_cast<unsigned>(single_character)
+        ROS_ERROR_STREAM("[ " << ros::this_node::getName().c_str() << "]: Message with bad checksum received, received: " << static_cast<unsigned>(single_character)
                               << ", calculated: " << static_cast<unsigned>(checksum));
         rec_state = WAITING_FOR_MESSSAGE;
       }
@@ -511,6 +224,33 @@ void BacaProtocol::serialDataCallback(uint8_t single_character) {
 }
 
 //}
+
+/* callbackSendMessage() //{ */
+
+void BacaProtocol::callbackSendMessage(const mrs_msgs::BacaProtocolConstPtr &msg) {
+
+  std::scoped_lock lock(mutex_msg);
+  uint8_t          payload_size = msg->payload.size();
+  uint8_t          tmp_send     = 'b';
+  uint8_t          checksum     = 0;
+
+  serial_port_->sendChar(tmp_send);
+  checksum += tmp_send;
+
+  serial_port_->sendChar(payload_size);
+  checksum += payload_size;
+
+  for (int i = 0; i < payload_size; i++) {
+    tmp_send = msg->payload[i];
+    checksum += tmp_send;
+    serial_port_->sendChar(tmp_send);
+  }
+  serial_port_->sendChar(checksum);
+}
+
+//}
+
+// | ------------------------ routines ------------------------ |
 
 /* processMessage() //{ */
 
@@ -556,6 +296,42 @@ void BacaProtocol::processMessage(uint8_t payload_size, uint8_t *input_buffer, u
     ROS_INFO("[%s]: published", ros::this_node::getName().c_str());
     baca_protocol_publisher_.publish(msg);
   }
+}
+
+//}
+
+/* releaseSerialLine() //{ */
+
+void BacaProtocol::releaseSerialLine(void) {
+
+  delete serial_port_;
+}
+
+//}
+
+/* connectToSensors() //{ */
+
+uint8_t BacaProtocol::connectToSensor(void) {
+
+  // Create serial port
+  serial_port_ = new serial_device::SerialPort();
+
+  // Set callback function for the serial ports
+  serial_data_callback_function_ = boost::bind(&BacaProtocol::callbackSerialData, this, _1);
+  serial_port_->setSerialCallbackFunction(&serial_data_callback_function_);
+
+  // Connect serial port
+  ROS_INFO("[%s]: Openning the serial port.", ros::this_node::getName().c_str());
+  if (!serial_port_->connect(portname_)) {
+    ROS_ERROR("[%s]: Could not connect to sensor.", ros::this_node::getName().c_str());
+    return 0;
+  }
+
+  ROS_INFO("[%s]: Connected to sensor.", ros::this_node::getName().c_str());
+
+  lastReceived = ros::Time::now();
+
+  return 1;
 }
 
 //}
